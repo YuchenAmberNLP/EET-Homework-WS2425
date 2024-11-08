caa246c535d06257f13f1c145d503abf380a2bda
yuchen <viveutvivasmyc@gmail.com>
2024-11-09 00:44:11 +0100

    modify train.py and param file

--- HW4/train.py
+++ HW4/train.py
@@ -36,40 +36,94 @@ class NaiveBayesClassifier:
         self.total_doc_per_class = defaultdict(int)
         self.class_prior = {}
         self.vocab = set()
-        self.discount = {}
+        self.discount = None
+        self.apriori_prob = {}
+        self.r_w_given_c = defaultdict(lambda: defaultdict(float))
+        self.r_w_given_c_sum = {}
+        self.alpha_c = {}
 
-    def calculate_discount(self, word_freq):
-        N1 = sum(1 for count in word_freq.values() if count == 1)
-        N2 = sum(1 for count in word_freq.values() if count == 2)
-        if N1 + 2 * N2 == 0:
-            return 0
-        return N1 / (N1 + 2 * N2)
-
-    def train(self, train_dir):
+    def import_dirs(self, train_dir):
         for class_name in os.listdir(train_dir):
             class_dir = os.path.join(train_dir, class_name)
             if not os.path.isdir(class_dir):
                 continue
 
             word_freq, total_words = get_word_freq(class_dir)
+
+            # self.total_doc_per_class[class_name] += len(os.listdir(class_dir))
+            # self.total_word_per_class[class_name] += total_words
+            # for word, freq in word_freq.items():
+            #     self.word_freq_per_class[class_name][word] += freq
+            #     self.vocab.add(word)
+
+
             self.word_freq_per_class[class_name] = word_freq
             self.total_word_per_class[class_name] = total_words
             self.total_doc_per_class[class_name] = len(os.listdir(class_dir))
             self.vocab.update(word_freq.keys())
 
-            self.discount[class_name] = self.calculate_discount(word_freq)
-
         total_docs = sum(self.total_doc_per_class.values())
-        for class_name in self.total_doc_per_class:
-            self.class_prior[class_name] = self.total_doc_per_class[class_name] / total_docs
+        self.class_prior = {c: count / total_docs for c, count in self.total_doc_per_class.items()}
 
+    def calculate_discount_and_smoothing(self):
+        N1 = 0
+        N2 = 0
+        for c, words in self.word_freq_per_class.items():
+            for w, freq in words.items():
+                if freq == 1:
+                    N1 += 1
+                elif freq == 2:
+                    N2 += 1
+        if N1 + 2 * N2 > 0:
+            self.discount = N1 / (N1 + 2 * N2)
+        else:
+            self.discount = 0
+        # print("discount calculated,", self.discount)
+
+        for class_name, word_freq in self.word_freq_per_class.items():
+            self.r_w_given_c[class_name] = {
+                word: max(0, freq - self.discount) for word, freq in word_freq.items()
+            }
+            self.r_w_given_c_sum[class_name] = sum(self.r_w_given_c[class_name].values())
+            total_words = self.total_word_per_class[class_name]
+            self.alpha_c[class_name] = (1 - self.r_w_given_c_sum[class_name] / total_words) if total_words > 0 else 0
+        #     print("alpha-c calculated, ", alpha_c)
+        # print("alpha and discount calculated.")
+
+    def calculate_apriori_prob(self):
+        total_count = sum(sum(words.values()) for words in self.word_freq_per_class.values())
+        for w in self.vocab:
+            freq_w = sum(self.word_freq_per_class[c][w] for c in self.word_freq_per_class)
+            self.apriori_prob[w] = freq_w / total_count if total_count > 0 else 0
+
+
+    def calculate_prob_with_smoothing(self, word, class_name):
+        total_words = self.total_word_per_class[class_name]
+        r_w_given_c = self.r_w_given_c[class_name].get(word, 0)
+        p_w_given_c = (r_w_given_c / total_words) if total_words > 0 else 0
+        p_w_backoff = self.apriori_prob.get(word, 0)
+
+        prob_with_smoothing = p_w_given_c + self.alpha_c[class_name] * p_w_backoff
+
+        return prob_with_smoothing
+
+
+        
     def save_params(self, paramfile_name):
+        word_prob_given_class = {}
+        for class_name in self.word_freq_per_class:
+            word_prob_given_class[class_name] = {
+                word: self.calculate_prob_with_smoothing(word, class_name)
+                for word in self.vocab
+            }
+            # 'word_freq_per_class': {k: dict(v) for k, v in self.word_freq_per_class.items()},
+            # 'total_word_per_class': dict(self.total_word_per_class),
+            # 'class_prior': dict(self.class_prior),
+            # 'vocab': list(self.vocab),
+            # 'discount': dict(self.discount),
         param_data = {
-            'word_freq_per_class': {k: dict(v) for k, v in self.word_freq_per_class.items()},
-            'total_word_per_class': dict(self.total_word_per_class),
-            'class_prior': dict(self.class_prior),
-            'vocab': list(self.vocab),
-            'discount': dict(self.discount)
+            'prob_per_class': self.class_prior,
+            'word_prob_given_class': word_prob_given_class
         }
         with open(paramfile_name, 'w', encoding='utf-8') as f:
             json.dump(param_data, f, ensure_ascii=False, indent=4)
@@ -79,6 +133,8 @@ if __name__ == "__main__":
     train_dir = sys.argv[1]
     paramfile_name = sys.argv[2]
     classifier = NaiveBayesClassifier()
-    classifier.train(train_dir)
+    classifier.import_dirs(train_dir)
+    classifier.calculate_discount_and_smoothing()
+    classifier.calculate_apriori_prob()
     classifier.save_params(paramfile_name)
     print('paramfile saved successfully.')
